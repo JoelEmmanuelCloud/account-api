86c5aba5ab67ae73cbac2bce1f7b1e199bb7d2ea
Joel Emmanuel <ejoel0035@gmail.com>
2023-07-20 15:22:44 +0100

    fix: fix a bug

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+require('express-async-errors');
+
+
 const app = express();
-const {validateSignup} = require('./validator');
+// const {validateSignup} = require('./validator');
 
 app.use(express.json());
+const connectDB = require('./database/connect');
+
+const notFoundMiddleware = require('./middleware/notFound');
+const errorHandlerMiddleware = require('./middleware/errorHandler');
 
 const port = process.env.PORT || 3000;
 
@@ -16,9 +24,12 @@ const port = process.env.PORT || 3000;
 //     res.send('Successfully signed up');
 // });
 
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const start = async () => { 
     try {
+        await connectDB(process.env.MONGO_URL);
         app.listen(port, console.log('Server listening on port 3000'));
     } catch (error) {
         console.log(error.message);
