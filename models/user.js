cf8381ecbc6b903317e57c17fa2d23a00ea4cf13
Joel Emmanuel <ejoel0035@gmail.com>
2023-07-21 12:28:28 +0100

    feat: create user model

---
+++ models/user.js
@@ -0,0 +1,59 @@
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required']
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    validate: {
+        validator:validator.isEmail, 
+        message: 'Provide a valid email address'
+    }
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required']
+  },
+
+  carType: {
+    type: String,
+    enum: ['SEDAN', 'SUV', 'HATCHBACK', 'TRUCK', 'OTHER'],
+    required: [true, 'Car type is required']
+  },
+  zipCode: {
+    type: String,
+    required: [true, 'Zip code is required']
+  },
+  city: {
+    type: String,
+    required: [true, 'City is required']
+  },
+  country: {
+    type: String,
+    required: [true, 'Country is required']
+  }
+});
+
+userSchema.pre('save', async function () {
+  // console.log(this.modifiedPaths());
+  // console.log(this.isModified('name'));
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
+};
+
+module.exports = mongoose.model('User', userSchema);
