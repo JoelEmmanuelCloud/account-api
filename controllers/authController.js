d1dde99c48d1cb406d72564737b8064ea9d8b237
Joel Emmanuel <ejoel0035@gmail.com>
2023-07-21 18:15:17 +0100

    feat: login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,12 +17,29 @@ const register = async (req, res) => {
     attachCookiesToResponse({res, user: tokenUser});
 
     
-    res.status(StatusCodes.CREATED).json({user: tokenUser, token});
+    res.status(StatusCodes.CREATED).json({user: tokenUser});
 
 };
 
-const login = async (req, res) => {
-    res.send('login users');
+    const login = async (req, res) => {
+        const { email, password } = req.body;
+      
+        if (!email || !password) {
+          throw new CustomError.BadRequestError('Please provide email and password');
+        }
+        const user = await User.findOne({ email });
+      
+        if (!user) {
+          throw new CustomError.UnauthenticatedError('Invalid Credentials');
+        }
+        const isPasswordCorrect = await user.comparePassword(password);
+        if (!isPasswordCorrect) {
+          throw new CustomError.UnauthenticatedError('Invalid Credentials');
+        }
+        const tokenUser = {firstName:user.firstName, lastName: user.lastName, userId:user._id};
+        attachCookiesToResponse({ res, user: tokenUser });
+      
+        res.status(StatusCodes.OK).json({ user: tokenUser });
 }
 const logout = async (req, res) => {
     res.send('logout users');
