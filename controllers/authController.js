34f5b5bd9fbcfb232431041cc9a62f538971c255
Joel Emmanuel <ejoel0035@gmail.com>
2023-07-21 15:23:27 +0100

    fix: modify controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,8 @@
 const User = require('../models/user');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const jwt = require('jsonwebtoken');
 // const { attachCookiesToResponse, createTokenUser } = require('../utils');
-
+const {createJWT} = require('../utils');
 
 const register = async (req, res) => {
     const {email} = req.body;
@@ -16,7 +15,14 @@ const register = async (req, res) => {
     
     const user = await User.create(req.body);
     const tokenUser = {firstName:user.firstName, lastName: user.lastName, userId:user._id}
-    const token = jwt.sign(tokenUser, 'jwtSecret', {expiresIn: '1d'});
+    const token = createJWT({payload:tokenUser});
+    
+    const oneDay = 1000 * 24 * 60 * 60;
+    
+    res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + oneDay),
+    });
     res.status(StatusCodes.CREATED).json({user: tokenUser, token});
 
 };
