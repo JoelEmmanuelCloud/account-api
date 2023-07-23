7f2cd830a14867b0a6587c70e019af2d4059ae43
Joel Emmanuel <ejoel0035@gmail.com>
2023-07-23 13:56:24 +0100

    setup joi schema for account update

---
+++ validations/index.js
@@ -0,0 +1,9 @@
+const createAccount = require('./createAccountValidator')
+const login = require('./loginValidator')
+const updateAccount = require('./updateAccountValidator')
+
+module.exports = {
+    createAccount,
+    login,
+    updateAccount
+}
---
+++ validations/updateAccountValidator.js
@@ -0,0 +1,15 @@
+const Joi = require('joi');
+
+const updateAccountJoiSchema = Joi.object({
+    firstName: Joi.string().trim().min(2).max(50),
+    lastName: Joi.string().trim().min(2).max(50),
+    email: Joi.string().trim().email().lowercase(),
+    password: Joi.string().min(8).max(100),
+    confirmPassword: Joi.string().min(8).max(100),
+    carType: Joi.string().valid('SEDAN', 'SUV', 'HATCHBACK', 'TRUCK', 'OTHER'),
+    zipCode: Joi.string().trim().allow(''),
+    city: Joi.string().trim().max(100),
+    country: Joi.string().trim().max(100),
+});
+
+module.exports = updateAccountJoiSchema;
