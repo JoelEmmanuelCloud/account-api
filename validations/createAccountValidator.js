a80241386df379c98d2a8a89fb9f0df6f82dd821
Joel Emmanuel <ejoel0035@gmail.com>
2023-07-26 08:03:05 +0100

    fix: fix joi schema

--- validations/createAccountValidator.js
+++ validations/createAccountValidator.js
@@ -1,17 +1,80 @@
 const Joi = require('joi')
 
 const createAccountJoiSchema = Joi.object({
-    firstName: Joi.string().trim().min(2).max(50).required(),
-    lastName: Joi.string().trim().min(2).max(50).required(),
-    email: Joi.string().trim().email().required(),
-    password: Joi.string().min(8).required(),
-    confirmPassword: Joi.valid(Joi.ref('password')).required(),
-    carType: Joi.string()
-        .valid('SEDAN', 'SUV', 'HATCHBACK', 'TRUCK', 'OTHER')
-        .required(),
-    zipCode: Joi.string().trim().allow('').required(),
-    city: Joi.string().trim().required(),
-    country: Joi.string().trim().required(),
+    firstName: Joi.string()
+        .trim()
+        .min(2)
+        .max(50)
+        .regex(/^[a-z]+$/)
+        .required()
+        .messages({
+            'string.min':
+                'First name should be at least {#limit} characters long.',
+            'string.max':
+                'First name cannot be longer than {#limit} characters.',
+            'string.regex': 'First name should contain only lowercase letters.',
+            'any.required': 'First name is required.',
+        }),
+    lastName: Joi.string()
+        .trim()
+        .min(2)
+        .max(50)
+        .regex(/^[a-z]+$/)
+        .required()
+        .messages({
+            'string.min':
+                'Last name should be at least {#limit} characters long.',
+            'string.max':
+                'Last name cannot be longer than {#limit} characters.',
+            'string.regex': 'Last name should contain only lowercase letters.',
+            'any.required': 'Last name is required.',
+        }),
+    email: Joi.string().trim().email().required().messages({
+        'string.email': 'Please provide a valid email address.',
+        'any.required': 'Email is required.',
+    }),
+    password: Joi.string()
+        .min(8)
+        .regex(/^[a-z0-9!@#$%^&*()\-_=+{};:'",.<>/?\\|[\]~`]+$/)
+        .required()
+        .messages({
+            'string.min':
+                'Password should be at least {#limit} characters long.',
+            'string.regex':
+                'Password should contain lowercase letters, numbers, and special characters.',
+            'any.required': 'Password is required.',
+        }),
+    confirmPassword: Joi.valid(Joi.ref('password')).required().messages({
+        'any.only': 'Confirm password must match the password.',
+        'any.required': 'Confirm password is required.',
+    }),
+    carType: Joi.string().required().messages({
+        'any.required': 'Car type is required.',
+    }),
+    zipCode: Joi.string().trim().allow('').required().messages({
+        'string.empty': 'Zip code cannot be empty.',
+        'any.required': 'Zip code is required.',
+    }),
+    city: Joi.string()
+        .trim()
+        .regex(/^[a-z]+$/)
+        .required()
+        .messages({
+            'string.pattern.base':
+                'City should contain only lowercase letters.',
+            'string.empty': 'City cannot be empty.',
+            'any.required': 'City is required.',
+        }),
+    country: Joi.string()
+        .trim()
+        .regex(/^[a-z]+$/)
+        .required()
+        .messages({
+            'string.pattern.base':
+                'Country should contain only lowercase letters.',
+            'string.empty': 'Country cannot be empty.',
+            'any.required': 'Country is required.',
+        }),
 }).options({ abortEarly: false })
 
 module.exports = createAccountJoiSchema
--- validations/updateAccountValidator.js
+++ validations/updateAccountValidator.js
@@ -1,15 +1,68 @@
 const Joi = require('joi')
 
 const updateAccountJoiSchema = Joi.object({
-    firstName: Joi.string().trim().min(2).max(50),
-    lastName: Joi.string().trim().min(2).max(50),
-    email: Joi.string().trim().email().lowercase(),
-    password: Joi.string().min(8).max(100),
-    confirmPassword: Joi.valid(Joi.ref('password')).required(),
-    carType: Joi.string().valid('SEDAN', 'SUV', 'HATCHBACK', 'TRUCK', 'OTHER'),
+    firstName: Joi.string()
+        .trim()
+        .min(2)
+        .max(50)
+        .regex(/^[a-z]+$/)
+        .messages({
+            'string.min':
+                'First name should be at least {#limit} characters long.',
+            'string.max':
+                'First name cannot be longer than {#limit} characters.',
+            'string.regex': 'First name should contain only lowercase letters.',
+        }),
+    lastName: Joi.string()
+        .trim()
+        .min(2)
+        .max(50)
+        .regex(/^[a-z]+$/)
+        .messages({
+            'string.min':
+                'Last name should be at least {#limit} characters long.',
+            'string.max':
+                'Last name cannot be longer than {#limit} characters.',
+            'string.regex': 'Last name should contain only lowercase letters.',
+        }),
+    email: Joi.string().trim().email().lowercase().messages({
+        'string.email': 'Please provide a valid email address.',
+    }),
+    password: Joi.string()
+        .min(8)
+        .max(100)
+        .regex(/^[a-z0-9!@#$%^&*()\-_=+{};:'",.<>/?\\|[\]~`]+$/)
+        .messages({
+            'string.min':
+                'Password should be at least {#limit} characters long.',
+            'string.max': 'Password cannot be longer than {#limit} characters.',
+            'string.regex':
+                'Password should contain lowercase letters, numbers, and special characters.',
+            'any.required': 'Password is required.',
+        }),
+    confirmPassword: Joi.valid(Joi.ref('password')).messages({
+        'any.only': 'Confirm password must match the password.',
+    }),
+    carType: Joi.string().trim(),
     zipCode: Joi.string().trim().allow(''),
-    city: Joi.string().trim().max(100),
-    country: Joi.string().trim().max(100),
+    city: Joi.string()
+        .trim()
+        .max(100)
+        .regex(/^[a-z]+$/)
+        .messages({
+            'string.pattern.base':
+                'City should contain only lowercase letters.',
+            'string.max': 'City cannot be longer than {#limit} characters.',
+        }),
+    country: Joi.string()
+        .trim()
+        .max(100)
+        .regex(/^[a-z]+$/)
+        .messages({
+            'string.pattern.base':
+                'Country should contain only lowercase letters.',
+            'string.max': 'Country cannot be longer than {#limit} characters.',
+        }),
 }).options({ abortEarly: false })
 
 module.exports = updateAccountJoiSchema
